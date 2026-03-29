2a3d411aeedc0435a2bb6c3f9503cd71ae65d538
SeptianProject <septianuwoe75@gmail.com>
2026-03-29 21:25:31 +0700

    refactor: standardize formatting of remotePatterns in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,38 +4,38 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**",
       },
       // CBT Backend Storage - Production
       {
-        protocol: 'https',
-        hostname: 'cbt-app.sagaradev.com',
-        port: '',
-        pathname: '/storage/**',
+        protocol: "https",
+        hostname: "cbt-app.dkb.or.id",
+        port: "",
+        pathname: "/storage/**",
       },
       // CBT Backend Storage - Development
       {
-        protocol: 'http',
-        hostname: 'cbt-app.me',
-        port: '8000',
-        pathname: '/storage/**',
+        protocol: "http",
+        hostname: "cbt-app.me",
+        port: "8000",
+        pathname: "/storage/**",
       },
       // Local development
       {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '8000',
-        pathname: '/storage/**',
+        protocol: "http",
+        hostname: "localhost",
+        port: "8000",
+        pathname: "/storage/**",
       },
       // Possible IP address
       {
-        protocol: 'http',
-        hostname: '127.0.0.1',
-        port: '8000',
-        pathname: '/storage/**',
+        protocol: "http",
+        hostname: "127.0.0.1",
+        port: "8000",
+        pathname: "/storage/**",
       },
     ],
   },
